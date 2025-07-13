f8cdc8a913a12811238eba45c220949c51961653
polat türk <polatturr52@gmail.com>
2025-07-14 00:55:37 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Portfolio.Models;
+using System.Diagnostics;
+
+namespace Portfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+      
+    }
+}
